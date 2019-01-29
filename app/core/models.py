70cffd1588c4694bbd97fcb581df87b65ff289bd
jedi1983 <ragunan@hotmail.com>
2019-01-29 19:41:10 +0700

    adding test model for Models and superuser

--- app/core/models.py
+++ app/core/models.py
@@ -7,8 +7,19 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Creates And Saves A new User"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError("User Must Have Email Address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates And Saves A New Super User"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
 
         return user
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -12,3 +12,25 @@ class ModelTests(TestCase):
                                                     password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test thet email domain name"""
+        email = "roland@HOTMAIL.com"
+        user = get_user_model().objects.create_user(email=email,
+                                                    password="test123")
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Testing email user invalid"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
+
+    def test_create_new_superuser(self):
+        """Test Creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'roland@hotmail.com',
+            'pass123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
